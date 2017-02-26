1cf711f1de8287ab208932c968d4cc3310ee4736
FriesW <william.james.fries@gmail.com>
2017-02-25 18:20:05 -0600

    Targeted Generator
    Modified generator such that it can now be either freerunning, or target
    specific exponents. Still a little crusty. IE: only way to exit targeted
    mode is with a keyboard interrupt.

--- generator/gen.py
+++ generator/gen.py
@@ -7,14 +7,19 @@ def gen(n, outDir):
 	if not os.path.exists(outDir):
 		os.makedirs(outDir)
 	
+	print "Calculating..."
 	n = 2**n - 1
+	print "Converting to string..."
 	s = str(n)
+	print "Getting base10 length of number..."
 	l = len(s)
 	
+	print "Store length"
 	f = open(os.path.join(outDir,'len'), 'w')
 	f.write(str(l))
 	f.close()
 	
+	print "Store chunks"
 	pos = 0
 	count = 0
 	while pos < l:
@@ -32,6 +37,8 @@ def gen(n, outDir):
 			f.write(chr(out))
 		count += 1
 		f.close()
+		
+	print "DONE!"
 			
 list = [2,3,5,7,13,17,19,31,61,89,107,127,521,607,1279,
  2203,2281,3217,4253,4423,9689,9941,11213,19937,
@@ -40,7 +47,16 @@ list = [2,3,5,7,13,17,19,31,61,89,107,127,521,607,1279,
  6972593,13466917,20996011,24036583,25964951,
  30402457,32582657,37156667,
  42643801, 43112609, 57885161, 74207281]
-for i in range(len(list)):
-	print i, "of", len(list)
-	print "Computing", list[i]
-	gen(list[i], "output")
+ 
+if raw_input("'Y' for freerun, else custom run: ") == 'Y':
+	for i in range(len(list)):
+		print i+1, "of", len(list)
+		print "Computing", list[i]
+		gen(list[i], "output")
+
+else:
+	while True:
+		i = int(raw_input("Array Index:"))
+		print i+1, "of", len(list)
+		print "Computing", list[i]
+		gen(list[i], "output")
