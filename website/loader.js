70b21a392dd4a85ad01a79827ede80bea1abbc9b
FriesW <william.james.fries@gmail.com>
2017-02-26 03:23:12 -0600

    JS Caching
    Saving over 4000 HTTP requests...

--- website/loader.js
+++ website/loader.js
@@ -22,29 +22,43 @@ function Loader(exponent) {
 		return this.length;
 	}
 	
+	this.dictionary = new Object();
+	
 	//fetchBlock
 	this.fetchBlock = function(block)
 	{
-		var req = new XMLHttpRequest();
-		req.open('GET', FILE_PREPEND + this.exponent + '/' + block, false);
-		req.overrideMimeType('text\/plain; charset=x-user-defined');
-		req.send(null);
-		
-		if(req.status === 200)
+		//console.log("HERE");
+		block = String(block);
+		if( this.dictionary[block] === undefined )
 		{
-			var response = req.responseText;
-			var outputString = "";
-			for(var i = 0; i < response.length; i++)
+			console.log("fetch new");
+			var req = new XMLHttpRequest();
+			req.open('GET', FILE_PREPEND + this.exponent + '/' + block, false);
+			req.overrideMimeType('text\/plain; charset=x-user-defined');
+			req.send(null);
+			
+			if(req.status === 200)
 			{
-				var front = (response.charCodeAt(i) & 0xF0) >> 4;
-				var back  = response.charCodeAt(i) & 0x0F;
-				
-				if (front < 10){ outputString += front; }
-				if (back  < 10){ outputString += back ; }
+				var response = req.responseText;
+				var outputString = "";
+				for(var i = 0; i < response.length; i++)
+				{
+					var front = (response.charCodeAt(i) & 0xF0) >> 4;
+					var back  = response.charCodeAt(i) & 0x0F;
+					
+					if (front < 10){ outputString += front; }
+					if (back  < 10){ outputString += back ; }
+				}
+				this.dictionary[block] = outputString;
+				return outputString;
 			}
-			return outputString;
+			return -1;
 		}
-		return -1;
+		else
+		{
+			console.log("cache hit");
+			return this.dictionary[block];
+		}
 	}
 	
 	//getString
