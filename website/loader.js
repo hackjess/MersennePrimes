e4cb7de03f4528f07c5973dfa7bc894fe979c5ef
FriesW <william.james.fries@gmail.com>
2017-02-25 18:16:34 -0600

    JS Progress
    Debugging and testing with own javascript. Need to setup a proper server
    enviroment for XMLHttp testing.

--- website/loader.js
+++ website/loader.js
@@ -1,5 +1,6 @@
 function fetchBlock(exponent, block)
 {
+	console.log("Fetching " + exponent + '/' + block);
 	var req = new XMLHttpRequest();
 	req.open('GET', '' + exponent + '/' + block, false);
 	req.overrideMimeType('text\/plain; charset=x-user-defined');
@@ -14,24 +15,28 @@ function fetchBlock(exponent, block)
 			var front = response.charcodeAt(i) & 0xF0 >> 4;
 			var back  = response.charcodeAt(i) & 0x0F;
 			
-			if (front < 10): outputString += front;
-			if (back  < 10): outputString += back ;
+			if (front < 10){ outputString += front; }
+			if (back  < 10){ outputString += back ; }
 		}
 		console.log("Fetched: " + outputString);
 		return outputString;
 	}
+	console.log("Failure");
 	return -1;
 }
 
 function fetchSize(exponent)
 {
+	console.log("Fetching size of " + exponent);
 	var req = new XMLHttpRequest();
 	req.open('GET', '' + exponent + '/len', false);
 	req.send(null);
 	
 	if(req.status === 200)
 	{
+		console.log("Fetched " + req.responseText);
 		return parseInt( req.responseText );
 	}
+	console.log("Failure");
 	return -1;
 }
--- website/main.js
+++ website/main.js
@@ -1,8 +1,21 @@
-BLOCK_SIZE = 10000;
+BLOCK_SIZE = 10000; //Number of characters!
+
+var position = 0;
+var subposition = 0.0;
+var mouseY = 0;
+var active = false;
+
+var blocks = {};
 
 $(document).ready(function(){
 	
 	var exponent = window.location.hash.replace(/\D/g,'');
+	
+	if( exponent == '' )
+	{
+		window.location.href = "index.html";
+	}
+	
 	var length = fetchSize(exponent);
 	
 	if( length == -1 )
@@ -10,11 +23,57 @@ $(document).ready(function(){
 		window.location.href = "index.html";
 	}
 	
-	var blockA_index = 0;
-	var blockA = fetchBlock(exponent, blockA_index);
+	update();
+	
+	$(document).mousemove(function(e){
+		if( !active )
+		{
+			mouseY = e.pageY;
+		}
+		else
+		{
+			var delta = e.pageY - mouseY;
+		}
+	});
+	
+	$(".scrollbar").mouseDown(function(){
+		alert(this.index());
+	});
 	
 	$(window).resize(function(){
-		
+		update();
 	});
 	
 });
+
+function update(){
+	
+	/*var block = Math.floor(position / BLOCK_SIZE);
+	if( !isInDict(blocks, block ) )
+		blocks[block] = fetchBlock(exponent, block);
+	if( !isInDict(blocks, block + 1) )
+		blocks[block + 1] = fetchBlock(exponent, block + 1);
+	
+	var frontOffset = position % charsPerLine();
+	
+	var row = Math.floor( position / charsPerLine() );
+	
+	$("#content-1").html(blocks[block]);
+	$("#content-2").html(blocks[block + 1]);*/
+}
+
+function charsPerLine(){
+	//TODO
+}
+
+function heightPerLine(){
+	//TODO
+}
+
+function isInDict(dict, name ) {
+	for(var i = 0, len = dict.length; i < len; i++) {
+		if( dict[ i ].key === name )
+			return true;
+	}
+	return false;
+}
